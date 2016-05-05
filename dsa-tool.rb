65da0408c790a310f1b08d344f84ee365392b45f
Kenneth Endfinger <k.endfinger@dglogik.com>
2016-05-04 20:22:47 -0400

    Fix SHIM

--- dsa-tool.rb
+++ dsa-tool.rb
@@ -24,7 +24,7 @@ class DsaTool < Formula
   def shim_script target
     <<-EOS.undent
       #!/bin/bash
-      dart "#{libexec}/#{target}" $@
+      exec dart "#{libexec}/#{target}" "${@}"
     EOS
   end
 
